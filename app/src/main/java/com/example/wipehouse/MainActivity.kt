4d65ce54bd8ece83d4bc190dff7e235dc82a043e
Ignaciolf <ignaciolopezfeliu@gmail.com>
2022-04-19 12:43:04 +0200

    - added MainActivity xml

--- app/src/main/java/com/example/wipehouse/MainActivity.kt
+++ app/src/main/java/com/example/wipehouse/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.wipehouse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var buttonCliente = findViewById<Button>(R.id.buttonCliente)
+        var buttonTrabajador = findViewById<Button>(R.id.buttonTrabajador)
+        var linearparte1 = findViewById<LinearLayout>(R.id.linearparte1)
+        var linearparte2 = findViewById<LinearLayout>(R.id.linearparte2)
+        var textViewparte1 = findViewById<TextView>(R.id.textViewparte1)
+        var textViewparte2 = findViewById<TextView>(R.id.textViewparte2)
+
     }
 }
