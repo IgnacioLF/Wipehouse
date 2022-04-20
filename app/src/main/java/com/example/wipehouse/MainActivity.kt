38ba9ce1318cb517330a669656dbdcf3c711adfb
sozzza <ignaciolopezfeliu@gmail.com>
2022-04-20 18:17:29 +0200

    -added firebase implementation -removed actionbar -added register activity

--- app/src/main/java/com/example/wipehouse/MainActivity.kt
+++ app/src/main/java/com/example/wipehouse/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.wipehouse
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.Window
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
