800494f83cd640f18eb1e4beb2fcef849fdf5deb
Ignaciolf <ignaciolopezfeliu@gmail.com>
2022-04-26 12:19:35 +0200

    -changed themes colors -added user_cuenta xml -added test fragment_user_cuenta.xml edit text prueba

--- app/src/main/java/com/example/wipehouse/User_cuenta.kt
+++ app/src/main/java/com/example/wipehouse/User_cuenta.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,12 +21,14 @@ class User_cuenta : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    lateinit var editTextTextNombre : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -34,7 +37,10 @@ class User_cuenta : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_cuenta, container, false)
+        var  root = inflater.inflate(R.layout.fragment_user_cuenta, container, false)
+        editTextTextNombre = root.findViewById(R.id.editTextTextNombre)
+        editTextTextNombre.setText("esto es una prueba")
+        return root
     }
 
     companion object {
